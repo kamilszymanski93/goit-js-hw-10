9261da5a729f772c0ef6fa16cf2151e811967047
Kamil Szymański <kamil313313@gmail.com>
2023-05-24 20:07:32 +0200

    HW10

---
+++ src/js/cat-api.js
@@ -0,0 +1,77 @@
+// const KEY =
+//   'live_idIZYnecHH32vszveU4i2ziq4wdKrE7fa9zUSVuW6nPGuGlzHOwUNGhn4t1Su8ey';
+const catBreeds =
+  'https://api.thecatapi.com/v1/breeds?api_key=live_idIZYnecHH32vszveU4i2ziq4wdKrE7fa9zUSVuW6nPGuGlzHOwUNGhn4t1Su8ey';
+catFilter = document.querySelector('.breed-select');
+const chosenCatInfo = 'https://api.thecatapi.com/v1/images/search';
+let loading;
+let errorInfo;
+loading = document.querySelector('.loader');
+errorInfo = document.querySelector('.error');
+const catCard = document.querySelector('.cat-info');
+function hideAlert(zmienna) {
+  zmienna.classList.add('hidden');
+}
+hideAlert(loading);
+hideAlert(errorInfo);
+function showAlert(zmienna) {
+  zmienna.classList.remove('hidden');
+}
+function pingUrl(url) {
+  return new Promise((resolve, reject) => {
+    fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          reject(errorInfo);
+        } else {
+          return response.json();
+        }
+      })
+      .then(data => {
+        resolve(data);
+      })
+      .catch(err => reject(err));
+  });
+}
+
+function fetchBreeds() {
+  pingUrl(catBreeds).then(data => {
+    const catChoices = data
+      .map(dataOne => `<option value='${dataOne.id}'>${dataOne.name}</option>`)
+      .join('');
+    catFilter.insertAdjacentHTML('afterbegin', catChoices);
+  });
+}
+fetchBreeds();
+
+function fetchCatByBreed(breedId) {
+  const catUrl = `${chosenCatInfo}?breed_ids=${breedId}`;
+  pingUrl(catUrl)
+    .then(data => {
+      const pictureLink = `<div><img src="${data[0].url}" class= "cat-pic"></div>`;
+      catCard.insertAdjacentHTML('afterbegin', pictureLink);
+    })
+    .catch(err => {
+      showAlert(errorInfo);
+    });
+  const catInfo = `https://api.thecatapi.com/v1/breeds/${breedId}`;
+  pingUrl(catInfo)
+    .then(data => {
+      const catDesciption = `<div class = "cat-txt"><h1>${data.name}</h1><p>${data.description}</p><h2>Temperament</h2><p>${data.temperament}</p></div>`;
+      catCard.insertAdjacentHTML('beforeend', catDesciption);
+    })
+    .catch(err => {
+      showAlert(errorInfo);
+    });
+}
+
+function handleFilterForm(e) {
+  showAlert(loading);
+  catCard.innerHTML = '';
+  fetchCatByBreed(e.target.value);
+  setTimeout(function () {
+    hideAlert(loading);
+  }, 500);
+}
+
+catFilter.addEventListener('change', handleFilterForm);
